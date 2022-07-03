8e17fe117c3ac0c462d7d4ceede63da72063c1f3
eggsnham07 <eggsnham07@gmail.com>
2022-07-03 23:47:52 +0000

    Update mode variable

--- Scripts/UI Scripts/DevStats.gd
+++ Scripts/UI Scripts/DevStats.gd
@@ -2,3 +2,7 @@ extends Control
 
 func _physics_process(_delta):
 	$Layer/Titles/FPS.text = String(Performance.get_monitor(Performance.TIME_FPS))
+	if Global.debug_mode:
+		$Layer/Titles/Mode.text = "Debug"
+	else:
+		$Layer/Titles/Mode.text = "Release"
